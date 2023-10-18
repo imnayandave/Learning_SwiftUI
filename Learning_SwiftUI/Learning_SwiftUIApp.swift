f12d81e81d6bd5064dee021d58efc6ebd1ee48d1
Nayan Dave <61861453+imnayandave@users.noreply.github.com>
2023-10-18 16:53:34 +0530

    Initial Commit

---
+++ Learning_SwiftUI/Learning_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_SwiftUIApp.swift
+//  Learning_SwiftUI
+//
+//  Created by Nayan Dave on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
